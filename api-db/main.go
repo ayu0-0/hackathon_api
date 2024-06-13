596b9db50492c5986393702551d2d82dd69b4776
ayu0-0 <ayuko09381002@icloud.com>
2024-06-13 21:59:14 +0900

    change api-db

--- api-db/main.go
+++ api-db/main.go
@@ -36,19 +36,16 @@ func init() {
 		fmt.Printf("読み込み出来ませんでした: %v", err)
 	}
 
-	mysqlUser := os.Getenv("MYSQLUSER")
-	mysqlUserPwd := os.Getenv("MYSQLUSERPASSWORD")
-	mysqlDatabase := os.Getenv("MYSQLDATABASE")
+	// DB接続のための準備
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPwd := os.Getenv("MYSQL_PWD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
-	// ①-2
-	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(localhost:3306)/%s", mysqlUser, mysqlUserPwd, mysqlDatabase))
-	if err != nil {
-		log.Fatalf("fail: sql.Open, %v\n", err)
-	}
-	// ①-3
-	if err := _db.Ping(); err != nil {
-		log.Fatalf("fail: _db.Ping, %v\n", err)
-	}
+	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+
+	_db, err := sql.Open("mysql", connStr)
+
 	db = _db
 }
 
